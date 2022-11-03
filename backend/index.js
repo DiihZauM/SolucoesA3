0d8903f65bff0be5b5f6deb86e284d30a6eb1e0d
ManuelVictorCS <manuelvictorcs@gmail.com>
2022-11-03 20:37:13 -0300

    Criação de API de pegar todos os users e de atualizar user pelo id

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const configurations = require('./src/configurations/firebase');
 const firebase = require('firebase');
+const { request } = require('express');
 
 firebase.initializeApp(configurations);
 const database = firebase.firestore();
@@ -15,6 +16,18 @@ app.post('/user/register', async (req, res) => {
     const data = req.body;
     await User.add(data);
     res.status(201).send({ msg: "Usuário criado com sucesso!" });
+})
+
+app.get('/get-all-users', async (req, res) => {
+    const snapshot = await User.get();
+    const listUsers = snapshot.docs.map((doc)=>({id: doc.id, ...doc.data()}));
+    res.send(listUsers);
+})
+
+app.put('/user/update/:id', async (req, res) => {
+    const id = req.params.id;
+    await User.doc(id).update(req.body);
+    res.send({msg: "Usuário atualizado com sucesso !"});
 })
 
 app.listen(3000, () => {
