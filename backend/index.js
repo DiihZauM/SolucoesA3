72291e52d44efcd609f4bc1640f040e2734fb57c
Victor <victorari856@gmail.com>
2022-11-07 16:02:49 -0300

    Sistema de login(Em andamento)

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const cors = require('cors');
 const configurations = require('./src/configurations/firebase');
 const firebase = require('firebase');
@@ -12,6 +13,18 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+//Palavra-chave para começar uma sessão
+app.use(session({secret:"teste123"}));
+
+//Login do usuário(Não finalizado)
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/src', express.static(path.join(__dirname, 'src')));
+app.set('frontend', path.join(__dirname, '/frontend'));
+
+
+
+//Registro de usuário
 app.post('/user/register', async (req, res) => {
     const data = req.body;
     await User.add(data);
@@ -23,6 +36,8 @@ app.get('/get-all-users', async (req, res) => {
     const listUsers = snapshot.docs.map((doc)=>({id: doc.id, ...doc.data()}));
     res.send(listUsers);
 })
+
+//Atualização de Usuário
 
 app.put('/user/update/:id', async (req, res) => {
     const id = req.params.id;
