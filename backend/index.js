9aabb31d925827f45373b1296f4ba114f1bc5101
Victor <victorari856@gmail.com>
2022-11-10 19:29:53 -0300

    Só testando

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const session = require('express-session');
 const cors = require('cors');
 const configurations = require('./src/configurations/firebase');
 const firebase = require('firebase');
@@ -13,16 +12,16 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-//Palavra-chave para começar uma sessão
-app.use(session({secret:"teste123"}));
-
-//Login do usuário(Em andamento)
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.use('/src', express.static(path.join(__dirname, 'src')));
-app.set('frontend', path.join(__dirname, '/frontend'));
-
-
+/*
+const pool = firebase.createPool({
+    host: DB_HOST,
+    user: DB_id,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });*/
 
 //Registro de usuário
 app.post('/user/register', async (req, res) => {
@@ -48,3 +47,6 @@ app.put('/user/update/:id', async (req, res) => {
 app.listen(3000, () => {
     console.log("Servidor ativo http://localhost:3000");
 })
+
+//Login do usuário
+
